3391a18b56d792c762311fcc9adeb43b4ac87d4f
denichinv <denichinv@gmail.com>
2024-11-01 18:05:59 +0000

    Add files via upload

---
+++ conditinal_statements/grades.py
@@ -0,0 +1,12 @@
+grade = float(input())
+
+if grade >= 5.50:
+    print('Excellent')
+elif grade >= 4.50:
+    print('Very Good')
+elif grade >= 3.50:
+    print('Good')
+elif grade >= 2.50:
+    print("Average")
+elif grade < 2.5:
+    print('Poor')
