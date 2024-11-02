7b81b6b1cbbb5ef4273b014179d760a7104d09c1
denichinv <denichinv@gmail.com>
2024-11-02 17:37:15 +0000

    Add files via upload

---
+++ first_steps/random_number_generator.py
@@ -0,0 +1,6 @@
+import random
+
+random_number = random.randint(1,100)
+converted_number = float(random_number)
+print(f"The random number:{random_number} is from {type(random_number)} data type.")
+print(f"The random number:{converted_number} is from {type(converted_number)} data type.")
